04c2145e872c617d52b71ada435b7f08f0be5a57
Ngobidi <chibuzormaxwell@gmail.com>
2024-04-25 06:21:17 -0700

    update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -7,15 +7,15 @@ def canUnlockAll(boxes):
     """valiates a module that returns if True, all box in
     boxes can be opened
     """
-    x = len(boxes)
+    n = len(boxes)
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
     while len(unseen_boxes) > 0:
         box-Idx = unseen_boxes.pop()
-        if not box-Idx or box-Idx >= x or box-Idx < 0:
+        if not box-Idx or box-Idx >= n or box-Idx < 0:
             continue
         if box-Idx not in seen_boxes:
             unseen_boxes = unseen_boxes.union(boxes[box-Idx])
             seen_boxes.add(box-Idx)
-    return x == len(seen_boxes)
+    return n == len(seen_boxes)
 """
