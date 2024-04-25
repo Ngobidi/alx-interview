64d300afb2185703496e1cf4fe2524bf6a9a3264
Ngobidi <chibuzormaxwell@gmail.com>
2024-04-25 06:18:19 -0700

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A python module for working with lockboxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """valiates a module that returns if True, all box in
+    boxes can be opened
+    """
+    x = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        box-Idx = unseen_boxes.pop()
+        if not box-Idx or box-Idx >= x or box-Idx < 0:
+            continue
+        if box-Idx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box-Idx])
+            seen_boxes.add(box-Idx)
+    return x == len(seen_boxes)
+"""
