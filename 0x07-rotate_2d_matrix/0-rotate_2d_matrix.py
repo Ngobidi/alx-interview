53ccef2c1061b2b1b8f03f4a26e76bfecdfda60e
ngobidi <chibuzormaxwell@gmail.com>
2024-06-12 06:19:48 -0700

    updated

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""2D matrix rotation format.
+"""2-D matrix rotation format.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2-D matrix in place.
+    """Rotates an m by n 2D matrix in place.
     """
     if type(matrix) != list:
         return
@@ -13,17 +13,17 @@ def rotate_2d_matrix(matrix):
     if not all(map(lambda x: type(x) == list, matrix)):
         return
     rows = len(matrix)
-    columns = len(matrix[0])
-    if not all(map(lambda x: len(x) == columns, matrix)):
+    cols = len(matrix[0])
+    if not all(map(lambda x: len(x) == cols, matrix)):
         return
-    v, h = 0, rows - 1
-    for a in range(columns * rows):
+    c, r = 0, rows - 1
+    for i in range(cols * rows):
         if i % rows == 0:
             matrix.append([])
-        if h == -1:
-            h = rows - 1
-            v += 1
-        matrix[-1].append(matrix[h][v])
-        if v == columns - 1 and h >= -1:
-            matrix.pop(h)
-        h -= 1
+        if r == -1:
+            r = rows - 1
+            c += 1
+        matrix[-1].append(matrix[r][c])
+        if c == cols - 1 and r >= -1:
+            matrix.pop(r)
+        r -= 1
