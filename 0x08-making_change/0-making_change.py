79133df08504d3544bd7bd049d633b3545ae5e20
Ngobidi <chibuzormaxwell@gmail.com>
2024-06-12 06:53:43 -0700

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""module for making_change.
+"""modules for making-change.
 """
 
 
 def makeChange(coins, total):
-    """evaluates the fewest num of coins needed to meet a given
-    amount total when given a pile of coins of different values.
+    """evaluate the fewest nums of coins needed to meet a given
+    amount total when given a piles of coins of different values.
     """
     if total <= 0:
         return 0
